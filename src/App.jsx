58d2941f1a663fa5ce86ebb3437f54ddd23a825d
sage-hex <realitynuel@gmail.com>
2025-04-22 17:33:00 +0100

    installed aos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import router from "./routes/router";
- 
 
-const App = () => <RouterProvider router={router} />;
+const App = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      offset: 100,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+      anchorPlacement: "top-bottom",
+    });
+  }, []);
+
+  return <RouterProvider router={router} />;
+};
 
 export default App;
