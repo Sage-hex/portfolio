6a8181c9ede417168b90f5723aa3fb807faa4891
sage-hex <realitynuel@gmail.com>
2025-04-22 22:37:21 +0100

    resolved the scrolling issues

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,49 +1,5 @@
 
-
-
-// import { Outlet } from "react-router-dom";
-// import Navbar from "./Navbar";
-// import ThemeToggle from "../../utils/ThemeToggle";
-// import MobileNav from "./MobileNav"; // Make sure this path is correct
-// import Footer from "./Footer";
-
-// const Layout = () => {
-//   return (
-//     <div className="min-h-screen text-black bg-[#f5f5f5]  dark:bg-[#0E0E0E] dark:text-white transition-colors duration-500 relative">
-//       {/* Glowing Bubbles */}
-      
-//       {/* Top Left Bubble */}
-//       <div className="absolute top-10 left-10 w-32 h-32 bg-gradient-to-br from-purple-400 via-pink-500 to-yellow-300 rounded-full blur-3xl opacity-30 animate-bubbleGlow z-0"></div>
-
-//       {/* Middle Right Bubble */}
-//       <div className="absolute top-1/2 right-10 w-32 h-32 bg-gradient-to-br from-blue-400 via-teal-500 to-green-300 rounded-full blur-3xl opacity-30 animate-bubbleGlow z-0 transform -translate-y-1/2"></div>
-
-//       {/* Bottom Bubble */}
-//       <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 w-36 h-36 bg-gradient-to-br from-indigo-400 via-violet-500 to-purple-500 rounded-full blur-3xl opacity-30 animate-bubbleGlow z-0"></div>
-
-//       {/* Toggle placed at top */}
-//       <div className="hidden md:flex justify-end p-4">
-//         {/* <ThemeToggle /> */}
-//       </div>
-
-//       <Navbar />
-
-//       <main className="flex flex-col items-center justify-center min-h-screen pb-20">
-//         <Outlet />
-//       </main>
-
-//       {/* Show Mobile Bottom Nav only on mobile */}
-//       <MobileNav />
-//       <Footer/>
-//     </div>
-//   );
-// };
-
-// export default Layout;
-
-
-
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, ScrollRestoration } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import ThemeToggle from "../../utils/ThemeToggle";
@@ -64,6 +20,9 @@ const Layout = () => {
     setTimeout(() => {
       handleStop();
     }, 3000);
+    
+    // Scroll to top when location changes
+    window.scrollTo(0, 0);
   }, [location]);
 
   return (
@@ -86,6 +45,7 @@ const Layout = () => {
 
       <MobileNav />
       <Footer/>
+      <ScrollRestoration /> {/* Add the ScrollRestoration component */}
     </div>
   );
 };
